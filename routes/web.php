64321aab5048ed6cd1e0993593c3d96b536301e6
alvin616 <nukhialvinrhmdni@gmail.com>
2026-03-06 06:49:12 +0700

    jgfdtdtm

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/nukhi', function () {
-    return view('nukhi');
+Route::get('/perkenalan', function () {
+    return '
+        <h1>Nama Saya Husain Aziz Al Rosyid</h1>
+        <p>NIM: 4124031 | Program Studi: Sistem Informasi</p>
+    ';
 });
-Route::get('/husain', function () {
-    return view('husain');
+Route::get('/nukhi', function () {
+    return '
+        <h1>Nama Saya Nukhi Alvin R</h1>
+        <p>NIM: 4124044 | Program Studi: Sistem Informasi</p>
+    ';
 });
