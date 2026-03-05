7d84ad6f56a8efdd05a8b8ff997d12faa09a87cf
alvin616 <nukhialvinrhmdni@gmail.com>
2026-03-05 12:08:13 +0700

    first commit

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Perkenalan</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+    </ul>
+    <p>Halo, saya Nukhi Alvin Rahmahdani</p>
+    <p>Nim: 4124044</p>
+</body>
+</html>
