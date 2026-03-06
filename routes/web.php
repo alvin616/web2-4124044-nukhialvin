af7eaf0ac301d2220d4c8e5172585e6b36bfde7b
HusainAziz2308 <husainazizalrosyid@gmail.com>
2026-03-06 07:21:54 +0700

    ganti sesuai petunjuk

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ Route::get('/', function () {
 });
 Route::get('/perkenalan', function () {
     return '
-        <h1>Nama Saya Husain Aziz Al Rosyid</h1>
-        <p>NIM: 4124031 | Program Studi: Sistem Informasi</p>
-    ';
-});
-Route::get('/nukhi', function () {
-    return '
         <h1>Nama Saya Nukhi Alvin R</h1>
         <p>NIM: 4124044 | Program Studi: Sistem Informasi</p>
+    ';
+});
+Route::get('/husain', function () {
+    return '
+        <h1>Nama Saya Husain Aziz Al Rosyid</h1>
+        <p>NIM: 4124031 | Program Studi: Sistem Informasi</p>
     ';
 });
